3f4907aed3d208031cd86576ce4528e09138857f
Pedro <costabreu.pedro@gmail.com>
2021-07-02 13:55:42 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Home(){
+
+    return(
+        <main>
+            <div className="box">
+
+            </div>
+            <footer></footer>
+        </main>  
+    );
+}
